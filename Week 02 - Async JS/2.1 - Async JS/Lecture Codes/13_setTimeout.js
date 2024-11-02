8c570c2785a5c3c1279c6164c2333da2e586a0bc
kaustav234 <kaustavgupta2311@gmail.com>
2024-11-02 16:49:21 +0530

    Async JS lecture codes added

--- Week 02 - Async JS/2.1 - Async JS/Lecture Codes/13_setTimeout.js
+++ Week 02 - Async JS/2.1 - Async JS/Lecture Codes/13_setTimeout.js
@@ -1,18 +1,26 @@
-function run() {
-	console.log("I will run after 1s");
-}
+// function run() {
+// 	console.log("I will run after 1s");
+// }
 
-setTimeout(run, 1000);
-// setTimeout is a global function which is available to everyone
+// setTimeout(run, 1000);
+// // setTimeout is a global function which is available to everyone
 
-console.log("I will run immedietely");
+// console.log("I will run immedietely");
 
-console.log("Hi!")
+// console.log("Hi!")
 
 function timeout() {
 	console.log("click the button")
 }
 
-setTimeout(timeout, 5000);
+setTimeout(timeout, 10000);
 
 console.log("welcome here")
+
+// 4-5f seconds
+let c = 0;
+for(let i = 0; i < 10000000000; i++) {
+	c+=1;
+}
+
+console.log("expensive operation done")
