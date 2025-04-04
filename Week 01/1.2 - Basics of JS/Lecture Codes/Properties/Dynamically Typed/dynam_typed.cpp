91d8e1cc86c30b9744972a46bc2964d90c78ab41
kaustav234 <kaustavgupta2311@gmail.com>
2025-04-04 23:32:27 +0530

    added properties

---
+++ Week 01/1.2 - Basics of JS/Lecture Codes/Properties/Dynamically Typed/dynam_typed.cpp
@@ -0,0 +1,9 @@
+int main() {
+    int a = 1; // C++ does not allow changing the type of a variable
+    // a = "hello";
+    // a = "true";
+
+    // here we were trying to change it from int to char within same line of code
+}
+
+
---
+++ Week 01/1.2 - Basics of JS/Lecture Codes/Properties/Dynamically Typed/dynamc_typed.js
@@ -0,0 +1,7 @@
+var a = 1;
+a = "kaustav";
+a = true;
+
+console.log(a)
+
+// here you can change the type in JAVA SCRIPT
