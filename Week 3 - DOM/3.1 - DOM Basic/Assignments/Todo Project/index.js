1a2553a0ade8169933d37bb9c107ce226fcb3529
kaustav234 <kaustavgupta2311@gmail.com>
2025-05-03 11:26:04 +0530

    added comments in todo script

--- Week 3 - DOM/3.1 - DOM Basic/Assignments/Todo Project/index.js
+++ Week 3 - DOM/3.1 - DOM Basic/Assignments/Todo Project/index.js
@@ -1,18 +1,24 @@
 let ctr = 1;
 function deleteTodo(index) {
+    // find the element by its id
     const element = document.getElementById(index);
+    // remove the element from its parent (the body)
     element.parentNode.removeChild(element);
 }
 
 function addTodo() {
+    // get the input element and extractss the value
     const Ele = document.querySelector("input");
     const ExtractedValue = Ele.value;
 
+    // create a new div element and set its id to the current value of ctr
     const newDivEle = document.createElement("div");
     newDivEle.setAttribute("id", ctr);
 
+    // set the innerHTML of the new div element to the extracted value and a delete button
     newDivEle.innerHTML = "<div>" + ExtractedValue + "</div> <button onclick='deleteTodo(" + ctr + ")'> Delete </button>"
 
+    // appendChild the new div element to the body of the document
     document.querySelector("body").appendChild(newDivEle);
     ctr += 1;
 }
