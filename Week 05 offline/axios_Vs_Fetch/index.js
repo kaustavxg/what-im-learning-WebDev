7f294eb132a88738a4059f7dfea83251d5af58cc
kaustavgupta <kaustavgupta2311@gmail.com>
2025-07-17 12:53:52 +0530

    week 5 code

---
+++ Week 05 offline/axios_Vs_Fetch/index.js
@@ -0,0 +1,12 @@
+// axios vs fetch 
+
+const { response } = require("express");
+
+function main(){
+    fetch("https://sum-server.100xdevs.com/todos")
+    .then(response => {
+        ``
+    } )
+}
+
+main();
