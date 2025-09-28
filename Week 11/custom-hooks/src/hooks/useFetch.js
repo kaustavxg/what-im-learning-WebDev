b3335ab1b81bbdbfc26a3010659d458df886ad4d
kaustavxg <kaustavgupta2311@gmail.com>
2025-09-28 22:59:13 +0530

    custom hook

--- Week 11/custom-hooks/src/hooks/useFetch.js
+++ Week 11/custom-hooks/src/hooks/useFetch.js
@@ -1,39 +1,47 @@
-import { json } from "express";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-//!    custom hook 
-export function usePostTitle(){
-    const [post, setPost] = useState({}) // empty object 
+// //!    custom hook 
+// export function usePostTitle(){
+//     const [post, setPost] = useState({});
 
-    async function getPost(){
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts/3");
-        const json = await response.json();
+//     async function getPosts(){
+//         const response = await fetch("https://jsonplaceholder.typicode.com/posts/3");
+//         const json = await response.json;
 
-        setPost(json);
-  }
+//         setPost(json);
+//     }
 
-  useEffect(() => {
-    getPost(); 
-  }, [])
+//     useEffect(() => {
+//         getPosts();
+//     }, [])
 
-  return post.title;
-}
+//     return post.title
+// }
 
-export function useFetch(url){
+export function useFetch(url, retryTime){
     const [finalData, setFinalData] = useState({});
+    const [loading, setLoading] = useState(true);
 
     async function getDetails(){
+        setLoading(true);
         const response = await fetch(url);
         const json = await response.json();
-        return json;
 
+        setFinalData(json);
+        setLoading(false);
     }
-    
+
     useEffect(() => {
         getDetails();
-    })
+    }, [url]); // dependency array, whenever it changes it mountes the getDetails() function for once
+    // if url nai chota toh don't go to the backend 
 
-    return {
-        finalData
-    }
+    useEffect(() => {
+        setInterval(getDetails, retryTime * 1000) // clean up
+    }, [])
+
+    return(
+        {finalData,
+        loading}
+    )
 }
